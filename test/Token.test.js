7de160181ee80b9dd98d541c8cc9ae4569707150
0xAshish <a4ashishrp@gmail.com>
2019-04-01 14:50:42 +0530

    add test for pausable

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,10 +1,5 @@
-const ethUtils = require("ethereumjs-util")
-
 const Token = artifacts.require("./contracts/Token.sol")
 const BigNumber = web3.utils.BigNumber
-const helper = require("./helper")
-
-const packageJSON = require("../package.json")
 
 const toWei = web3.utils.toWei
 
@@ -15,14 +10,11 @@ require("chai")
 contract("Token", async accounts => {
   let owner = accounts[0]
   let token
-  let Wallets
 
   before(async function() {
     token = await Token.new("Matic test", "MTX", 18, toWei("100"), {
       from: owner
     })
-
-    Wallets = helper.generateFirstWallets(packageJSON.config.mnemonics, 10)
   })
 
   describe("total supply", function() {
@@ -39,69 +31,21 @@ contract("Token", async accounts => {
       assert.equal(balance.toString(), amount.toString())
     })
 
-    // it("should test token meta transfer", async function() {
-    //   const amount = toWei("5")
-    //   const to = accounts[2]
-    //   const nonce = 0
-    //   const reward = toWei("2")
-    //   //keccak256(abi.encodePacked(address(this), "metaTransfer", to, value, nonce, reward));
-    //   const data = Buffer.concat([
-    //     ethUtils.toBuffer(token.address),
-    //     ethUtils.toBuffer("metaTransfer"),
-    //     ethUtils.toBuffer(to),
-    //     ethUtils.setLengthLeft(+amount, 32),
-    //     ethUtils.setLengthLeft(nonce, 32),
-    //     ethUtils.setLengthLeft(+reward, 32)
-    //   ])
-
-    //   const sig = helper.getSig({
-    //     pk: Wallets[0].getPrivateKeyString(),
-    //     data: data
-    //   })
-
-    //   //bytes memory signature, address to, uint256 value, uint256 nonce, uint256 reward
-    //   await token.metaTransfer(sig, to, amount, nonce, reward, {
-    //     from: accounts[3]
-    //   })
-
-    //   let balance = await token.balanceOf(accounts[2])
-    //   balance.should.be.bignumber.equal(amount)
-    //   balance = await token.balanceOf(accounts[3])
-    //   balance.should.be.bignumber.equal(reward)
-    // })
-
-    // it("should test token meta approve", async function() {
-    //   const amount = toWei("5")
-    //   const to = accounts[2]
-    //   const nonce = 1
-    //   const reward = toWei("2")
-
-    //   //abi.encodePacked(address(this), "metaApprove", spender, value, nonce, reward)
-    //   const data = Buffer.concat([
-    //     ethUtils.toBuffer(token.address),
-    //     ethUtils.toBuffer("metaApprove"),
-    //     ethUtils.toBuffer(to),
-    //     ethUtils.setLengthLeft(+amount, 32),
-    //     ethUtils.setLengthLeft(nonce, 32),
-    //     ethUtils.setLengthLeft(+reward, 32)
-    //   ])
-
-    //   const sig = helper.getSig({
-    //     pk: Wallets[0].getPrivateKeyString(),
-    //     data: data
-    //   })
-    //   // address spender, uint256 value, uint256 nonce, uint256 reward, bytes memory signature
-    //   await token.metaApprove(to, amount, nonce, reward, sig, {
-    //     from: accounts[4]
-    //   })
-    //   const allowance = await token.allowance(
-    //     Wallets[0].getAddressString(),
-    //     accounts[2]
-    //   )
-    //   allowance.should.be.bignumber.equal(amount)
-
-    //   const balance = await token.balanceOf(accounts[4])
-    //   balance.should.be.bignumber.equal(reward)
-    // })
+    it("should pause test token and check for correctness", async function() {
+      await token.pause({ from: accounts[0] })
+      const amount = toWei("1")
+      try {
+        await token.transfer(accounts[2], amount)
+      } catch (error) {
+        const invalidOpcode = error.message.search("revert") >= 0
+        assert(invalidOpcode, "Expected revert, got '" + error + "' instead")
+      }
+      let balance = await token.balanceOf.call(accounts[2])
+      assert.equal(balance.toString(), "0")
+      await token.unpause({ from: accounts[0] })
+      await token.transfer(accounts[2], amount)
+      balance = await token.balanceOf.call(accounts[2])
+      assert.equal(balance.toString(), amount.toString())
+    })
   })
 })
