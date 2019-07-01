8e9577205ba53ff9900445ff25bb3fe878d8c846
atvanguard <93arpit@gmail.com>
2019-07-01 12:20:08 +0530

    estimate gas for firing tx

--- scripts/airdrop.js
+++ scripts/airdrop.js
@@ -9,12 +9,19 @@ async function fireTx() {
   let { recipients, amounts, airdropSupply } = await parseCsv(web3)
   const airdrop = await Airdrop.at(process.env.AIRDROP_CONTRACT)
   const token = await Token.at(process.env.TOKEN_CONTRACT)
+
   airdropSupply = web3.utils.toBN(airdropSupply).mul(SCALING_FACTOR)
   let airdropContractBalance = await token.balanceOf.call(airdrop.address)
   console.log('airdropContractBalance', airdropContractBalance.toString())
   assert.ok(airdropContractBalance.eq(airdropSupply), 'airdropSupply assertion failed')
+  assert.ok(airdropContractBalance.eq(web3.utils.toBN(48198).mul(SCALING_FACTOR)), 'airdropSupply assertion failed')
 
-  return airdrop.airdropTokens(recipients, amounts)
+  const gasEstimate = await airdrop.airdropTokens.estimateGas(recipients, amounts)
+  console.log('gasEstimate', gasEstimate)
+  return airdrop.airdropTokens(recipients, amounts, {
+    gas: gasEstimate + 10000,
+    gasPrice: web3.utils.toWei('2', 'gwei')
+  })
 }
 
 module.exports = async function(callback) {
